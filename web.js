b03ff57cd26fe321bc9dfb8e7aec02b0cce40653
Andrey Petrov <apetrov.ya@yandex.ru>
2016-09-11 02:57:18 +0200

    Corrected errors on the server file

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -5,7 +7,7 @@ let PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(req,res)) {
+app.get('/', function(req,res) {
         res.sendFile(__dirname + '/dist/index.html');
     });
 
