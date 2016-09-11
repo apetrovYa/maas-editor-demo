13b2501c9948b7a872aeb7ef7afc07f65f568e9c
Andrey Petrov <apetrov.ya@yandex.ru>
2016-09-11 02:49:50 +0200

    Modified the server web file for heroku -- becouse the last file was deprecated

--- web.js
+++ web.js
@@ -1,7 +1,18 @@
-const gzippo  = require('gzippo');
+const path = require('path');
 const express = require('express');
 const app = express();
+let PORT = process.env.PORT || 8080;
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(req,res)) {
+        res.sendFile(__dirname + '/dist/index.html');
+    });
+
+app.listen(PORT, function(error){
+    if (error) {
+        console.log(error);
+        } else {
+            console.log("===> [] Listending on port $s.", PORT);
+            }
+    });    
